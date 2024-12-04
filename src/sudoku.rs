f31e7dd12b2af30f62fc6192d621e5fb44d05cbe
Hugo Lindström <huggepugge1@gmail.com>
2024-12-04 15:21:12 +0100

    get functions

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -9,10 +9,45 @@ impl Sudoku {
         for row in 0..9 {
             rows.push(Vec::new());
             for col in 0..9 {
-                rows[row].push(self.squares[col])
+                rows[row].push(self.squares[row * 9 + col])
             }
         }
 
         rows
+    }
+
+    pub fn get_cols(&self) -> Vec<Vec<u8>> {
+        let mut cols = Vec::new();
+        for col in 0..9 {
+            cols.push(Vec::new());
+            for row in 0..9 {
+                cols[col].push(self.squares[row * 9 + col])
+            }
+        }
+
+        cols
+    }
+
+    pub fn get_square(&self) -> Vec<Vec<u8>> {
+        let mut squares = Vec::new();
+        for square_row in 0..3 {
+            for square_col in 0..3 {
+                squares.push(Vec::new());
+                for row in 0..3 {
+                    for col in 0..3 {
+                        squares[square_row * 3 + square_col]
+                            .push(self.squares[square_row * 27 + square_col * 3 + row * 9 + col])
+                    }
+                }
+            }
+        }
+        squares
+    }
+
+    pub fn back_prop(&self) -> Self {
+        let new_squares = self.squares.clone();
+        Sudoku {
+            squares: new_squares,
+        }
     }
 }
